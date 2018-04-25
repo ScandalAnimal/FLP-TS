7b2ce4da6bb84f89376361ff4b5e7456aa93dde5
Maros Vasilisin <xvasil02@vutbr.cz>
2018-04-25 18:03:31 +0200

    few test cases

---
+++ test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+
+function compare
+{
+	input=$1
+	name=$(basename "$input")
+
+	./flp18-log < $input > "./outputs/$name"
+    var=$(comm -3 ./outputs/$name ./refs/$name)
+
+   	if [ -z "$var" ]
+	then
+    	nd="\033[1;32m${bold}PASS${normal}\033[0m"
+	else
+    	nd="\033[1;31m${bold}FAIL${normal}\033[0m"
+	fi
+
+	echo -e  "Processed $name: $nd"
+
+}
+
+function compareWrong
+{
+	input=$1
+	name=$(basename "$input")
+
+	./flp18-log < $input >> /dev/null 2>&1 
+  
+   	if [ $? == 0 ]
+	then
+		nd="\033[1;31m${bold}FAIL${normal}\033[0m"
+	else
+    	nd="\033[1;32m${bold}PASS${normal}\033[0m"
+	fi
+
+	echo -e "Processed $name: $nd"
+}
+
+rm -R -f ./outputs
+mkdir outputs
+
+echo -e "\033[0;35m${bold}Starting deterministic tests...${normal}\033[0m"
+for input in $(ls ./tests/*) ; do
+		compare $input
+done
+
+echo -e "\033[0;35m${bold}Starting wrong inputs tests...${normal}\033[0m"
+for input in $(ls ./wrongInputs/*) ; do
+		compareWrong $input
+done
